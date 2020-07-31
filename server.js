6e215ead047fa78e09b81908606e9b1e0a88a148
Ubuntu <ubuntu@vps-7869ac6d.vps.ovh.net>
2020-07-31 15:44:52 +0000

    Fixed audit errors on packages. Added support for API endpoint auth (set API_KEY in .env file). Added support for a users.json for limiting access to the server. Improved a bit of error checking

--- server.js
+++ server.js
@@ -14,166 +14,235 @@ import ClientManager from './lib/ClientManager';
 const debug = Debug('localtunnel:server');
 
 export default function(opt) {
-    opt = opt || {};
+	opt = opt || {};
 
-    const validHosts = (opt.domain) ? [opt.domain] : undefined;
-    const myTldjs = tldjs.fromUserSettings({ validHosts });
-    const landingPage = opt.landing || 'https://cloudkit.app';
+	// Constants used
+	const validHosts = (opt.domain) ? [opt.domain] : undefined;
+	const myTldjs = tldjs.fromUserSettings({ validHosts });
+	const landingPage = opt.landing || 'https://cloudkit.app';
 
-    function GetClientIdFromHostname(hostname) {
-        return myTldjs.getSubdomain(hostname);
-    }
+	const manager = new ClientManager(opt);
 
-    const manager = new ClientManager(opt);
+	const schema = opt.secure ? 'https' : 'http';
 
-    const schema = opt.secure ? 'https' : 'http';
+	const app = new Koa();
+	const router = new Router();
 
-    const app = new Koa();
-    const router = new Router();
+	// Lookup a hostname based on the client id
+	function GetClientIdFromHostname(hostname) {
+		return myTldjs.getSubdomain(hostname);
+	}
 
-    router.get('/api/status', async (ctx, next) => {
-        const stats = manager.stats;
-        ctx.body = {
-            tunnels: stats.tunnels,
-            mem: process.memoryUsage(),
-        };
-    });
+	// If requested we can have an api key
+	function checkAPI(ctx) {
+		if (process.env.API_KEY !== undefined && ctx.request.headers['x-api-key'] != process.env.API_KEY){
+			return false;
+		}
+		return true;
+	}
 
-    router.get('/api/tunnels/:id/status', async (ctx, next) => {
-        const clientId = ctx.params.id;
-        const client = manager.getClient(clientId);
-        if (!client) {
-            ctx.throw(404);
-            return;
-        }
+	function checkUserLogin(ctx){
+		if (process.env.USERSFILE  !== undefined){
+			// Do we have the file
+			if (!fs.existsSync(process.env.USERSFILE)){
+				debug('"%s" file not found',process.env.USERSFILE);
+				process.exit(1);
+				return false
+			}
+			// Do we have the user header
+			if (ctx.request.headers['x-user-key'] === undefined){
+				debug('x-user-key header is missing!');
+				return false;
+			}
 
-        const stats = client.stats();
-        ctx.body = {
-            connected_sockets: stats.connectedSockets,
-        };
-    });
+			const usrid = ctx.request.headers['x-user-key'];
+			let users = '';
+			//try read the file
+			try {
+				var data = fs.readFileSync(process.env.USERSFILE);
+				users = JSON.parse(data);
+			} catch (err) {
+				debug('User failed - unable to read/parse the users file');
+				process.exit(1);
+				return false;
+			}
 
-    app.use(router.routes());
-    app.use(router.allowedMethods());
+			// Did we find the user
+			if (users.hasOwnProperty(usrid)){
+				debug('User approved - success');
+				return true;
+			}
+			debug('User not found - failure');
+			return false;
+		}
+		return true;
+	}
 
-    // root endpoint
-    app.use(async (ctx, next) => {
-        const path = ctx.request.path;
 
-        // skip anything not on the root path
-        if (path !== '/') {
-            await next();
-            return;
-        }
+	router.get('/api/status', async (ctx, next) => {
+		if (checkAPI(ctx)){
+			debug('/api/status was blocked for %s',ctx.request.ip);
+			ctx.throw(403, 'Forbidden');
+		}
+		const stats = manager.stats;
+		ctx.body = {
+			tunnels: stats.tunnels,
+			mem: process.memoryUsage(),
+		};
+	});
 
-        const isNewClientRequest = ctx.query['new'] !== undefined;
-        if (isNewClientRequest) {
-            const reqId = hri.random();
-            debug('making new client with id %s', reqId);
-            const info = await manager.newClient(reqId);
+	router.get('/api/tunnels/:id/status', async (ctx, next) => {
+		if (checkAPI(ctx)){
+			debug('/api/tunnels/:id/status was blocked for %s',ctx.request.ip);
+			ctx.throw(403, 'Forbidden');
+		}
+		const clientId = ctx.params.id;
+		const client = manager.getClient(clientId);
+		if (!client) {
+			ctx.throw(404);
+			return;
+		}
 
-            const url = schema + '://' + info.id + '.' + ctx.request.host;
-            info.url = url;
-            ctx.body = info;
-            return;
-        }
+		const stats = client.stats();
+		ctx.body = {
+			connected_sockets: stats.connectedSockets,
+		};
+	});
 
-        // no new client request, send to landing page
-        ctx.redirect(landingPage);
-    });
 
-    // anything after the / path is a request for a specific client name
-    // This is a backwards compat feature
-    app.use(async (ctx, next) => {
-        const parts = ctx.request.path.split('/');
+	app.use(router.routes());
+	app.use(router.allowedMethods());
 
-        // any request with several layers of paths is not allowed
-        // rejects /foo/bar
-        // allow /foo
-        if (parts.length !== 2) {
-            await next();
-            return;
-        }
+	// root endpoint
+	app.use(async (ctx, next) => {
+		const path = ctx.request.path;
 
-        const reqId = parts[1];
+		// skip anything not on the root path
+		if (path !== '/') {
+			await next();
+			return;
+		}
 
-        // limit requested hostnames to 63 characters
-        if (! /^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
-            const msg = 'Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.';
-            ctx.status = 403;
-            ctx.body = {
-                message: msg,
-            };
-            return;
-        }
+		const isNewClientRequest = ctx.query['new'] !== undefined;
+		if (isNewClientRequest) {
+			if (!checkUserLogin(ctx)){
+				ctx.throw(403, 'Forbidden');
+				return;
+			}
 
-        debug('making new client with id %s', reqId);
-        const info = await manager.newClient(reqId);
+			const reqId = hri.random();
+			debug('making new client with id %s', reqId);
+			const info = await manager.newClient(reqId);
 
-        const url = schema + '://' + info.id + '.' + ctx.request.host;
-        info.url = url;
-        ctx.body = info;
-        return;
-    });
+			const url = schema + '://' + info.id + '.' + ctx.request.host;
+			info.url = url;
+			ctx.body = info;
+			return;
+		}
 
-let server;
-if (opt.secure) {
-  server = https.createServer({
-                                      key: fs.readFileSync(process.env.SSL_KEY),
-                                      cert: fs.readFileSync(process.env.SSL_CERT)
-                                    });
-} else {
-  server = http.createServer()
-}
+		// no new client request, send to landing page
+		ctx.redirect(landingPage);
+	});
 
-    const appCallback = app.callback();
+	// anything after the / path is a request for a specific client name
+	// This is a backwards compat feature
+	app.use(async (ctx, next) => {
+		const parts = ctx.request.path.split('/');
 
-    server.on('request', (req, res) => {
-        // without a hostname, we won't know who the request is for
-        const hostname = req.headers.host;
-        if (!hostname) {
-            res.statusCode = 400;
-            res.end('Host header is required');
-            return;
-        }
+		// any request with several layers of paths is not allowed
+		// rejects /foo/bar
+		// allow /foo
+		if (parts.length !== 2) {
+			await next();
+			return;
+		}
 
-        const clientId = GetClientIdFromHostname(hostname);
-        if (!clientId) {
-            appCallback(req, res);
-            return;
-        }
+		if (!checkUserLogin(ctx)){
+			ctx.throw(403, 'Forbidden');
+			return;
+		}
 
-        const client = manager.getClient(clientId);
-        if (!client) {
-            res.statusCode = 404;
-            res.end('404');
-            return;
-        }
+		const reqId = parts[1];
 
-        client.handleRequest(req, res);
-    });
+		// limit requested hostnames to 63 characters
+		if (! /^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
+			const msg = 'Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.';
+			ctx.status = 403;
+			ctx.body = {
+				message: msg,
+			};
+			return;
+		}
 
-    server.on('upgrade', (req, socket, head) => {
-        const hostname = req.headers.host;
-        if (!hostname) {
-            socket.destroy();
-            return;
-        }
+		debug('making new client with id %s', reqId);
+		const info = await manager.newClient(reqId);
 
-        const clientId = GetClientIdFromHostname(hostname);
-        if (!clientId) {
-            socket.destroy();
-            return;
-        }
+		const url = schema + '://' + info.id + '.' + ctx.request.host;
+		info.url = url;
+		ctx.body = info;
+		return;
+	});
 
-        const client = manager.getClient(clientId);
-        if (!client) {
-            socket.destroy();
-            return;
-        }
+	let server;
+	if (opt.secure && fs.existsSync(process.env.SSL_KEY) && fs.existsSync(process.env.SSL_CERT)) {
+		debug('Running secure server, https using %s , %s',process.env.SSL_KEY,process.env.SSL_CERT);
+		server = https.createServer({
+			key: fs.readFileSync(process.env.SSL_KEY, 'ascii'),
+			cert: fs.readFileSync(process.env.SSL_CERT, 'ascii')
+		});
+	} else {
+		server = http.createServer();
+		debug('Running insecure server, http');
+	}
 
-        client.handleUpgrade(req, socket);
-    });
+	const appCallback = app.callback();
 
-    return server;
+	server.on('request', (req, res) => {
+		// without a hostname, we won't know who the request is for
+		const hostname = req.headers.host;
+		if (!hostname) {
+			res.statusCode = 400;
+			res.end('Host header is required');
+			return;
+		}
+
+		const clientId = GetClientIdFromHostname(hostname);
+		if (!clientId) {
+			appCallback(req, res);
+			return;
+		}
+
+		const client = manager.getClient(clientId);
+		if (!client) {
+			res.statusCode = 404;
+			res.end('404');
+			return;
+		}
+
+		client.handleRequest(req, res);
+	});
+
+	server.on('upgrade', (req, socket, head) => {
+		const hostname = req.headers.host;
+		if (!hostname) {
+			socket.destroy();
+			return;
+		}
+
+		const clientId = GetClientIdFromHostname(hostname);
+		if (!clientId) {
+			socket.destroy();
+			return;
+		}
+
+		const client = manager.getClient(clientId);
+		if (!client) {
+			socket.destroy();
+			return;
+		}
+
+		client.handleUpgrade(req, socket);
+	});
+
+	return server;
 };
